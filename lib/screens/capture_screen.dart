30c9f92e7722af353b876a7226b95a13e811fda9
J Aravindhan <thearuvi@gmail.com>
2021-09-01 23:18:16 +0530

    added profile screen

--- lib/screens/capture_screen.dart
+++ lib/screens/capture_screen.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:travel_explorer/model/souvenir_list.dart';
 
 class ItemDetailScreen extends StatefulWidget {
-
   final ItemsData selectedItemData;
 
-  ItemDetailScreen({Key key, @required this.selectedItemData}) : super(key: key);
+  ItemDetailScreen({Key key, @required this.selectedItemData})
+      : super(key: key);
 
   @override
-  State<ItemDetailScreen> createState() => ItemDetailState(objSelectedItemData: this.selectedItemData);
+  State<ItemDetailScreen> createState() =>
+      ItemDetailState(objSelectedItemData: this.selectedItemData);
 }
 
-class ItemDetailState extends State<ItemDetailScreen> with TickerProviderStateMixin {
-
+class ItemDetailState extends State<ItemDetailScreen>
+    with TickerProviderStateMixin {
   ItemsData objSelectedItemData;
 
   ItemDetailState({this.objSelectedItemData});
@@ -38,38 +39,28 @@ class ItemDetailState extends State<ItemDetailScreen> with TickerProviderStateMi
     _controller = AnimationController(
       duration: const Duration(seconds: 3),
       vsync: this,
-
-    )..repeat(reverse:true);
-    _animation = CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeIn
-    );
+    )..repeat(reverse: true);
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
 
     Future.delayed(const Duration(milliseconds: 500), () {
-
       print("Delay Binding");
       setState(() {
         _angleArrow = 50;
       });
-
     });
 
     Future.delayed(const Duration(milliseconds: 1500), () {
-
       print("Delay Binding");
       setState(() {
         _angleArrow = 120;
       });
-
     });
 
     Future.delayed(const Duration(milliseconds: 2500), () {
-
       print("Delay Binding");
       setState(() {
         _angleArrow = 200;
       });
-
     });
   }
 
@@ -91,53 +82,66 @@ class ItemDetailState extends State<ItemDetailScreen> with TickerProviderStateMi
       ),
       home: Scaffold(
           body: Column(
-            children: <Widget>[
+        children: <Widget>[
           Container(
-          color: Colors.white,
+            color: Colors.white,
             child: FadeTransition(
               opacity: _animation,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.network("https://static4.depositphotos.com/1000441/322/i/600/depositphotos_3226927-stock-photo-eiffel-tower-in-paris.jpg"),
+                  Image.network(
+                      "https://static4.depositphotos.com/1000441/322/i/600/depositphotos_3226927-stock-photo-eiffel-tower-in-paris.jpg"),
                 ],
               ),
             ),
           ),
-              Expanded(
-                  child:Container(
-                    height: 200,
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text('${objSelectedItemData.distanceFromUserLocation}', style: TextStyle(fontSize: 100, color: Colors.lightBlue,fontWeight: FontWeight.bold)),
-                                Text(' km', style: TextStyle(fontSize: 50,color: Colors.lightBlue,fontWeight: FontWeight.normal))
-                              ]),
-                  )),
-              Container(
-                height: 200,
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(25),
-                color: Colors.white,
-                child:_loadBottomActionWidget(),
-              ),
-            ],
-          )),);
+          Expanded(
+              child: Container(
+            height: 200,
+            width: double.infinity,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(10),
+            color: Colors.white,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('${objSelectedItemData.distanceFromUserLocation}',
+                      style: TextStyle(
+                          fontSize: 100,
+                          color: Colors.lightBlue,
+                          fontWeight: FontWeight.bold)),
+                  Text(' km',
+                      style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.lightBlue,
+                          fontWeight: FontWeight.normal))
+                ]),
+          )),
+          Container(
+            height: 200,
+            width: double.infinity,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(25),
+            color: Colors.white,
+            child: _loadBottomActionWidget(),
+          ),
+        ],
+      )),
+    );
   }
 
   Widget _loadBottomActionWidget() {
-    if(objSelectedItemData.distanceFromUserLocation>10)
-      {
-        return Text('You have to be there around 1 km radius from the destination', style: TextStyle(fontSize: 18,color: Colors.blueGrey,fontWeight: FontWeight.bold));
-      }
-    else {
+    if (objSelectedItemData.distanceFromUserLocation > 10) {
+      return Text(
+          'You have to be there around 1 km radius from the destination',
+          style: TextStyle(
+              fontSize: 18,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold));
+    } else {
       return MaterialButton(
         onPressed: () {},
         shape: const StadiumBorder(),
